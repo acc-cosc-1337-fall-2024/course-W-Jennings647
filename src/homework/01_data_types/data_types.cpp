bda2e64483346c26e0d1aab627a4cd8f414d049d
William Jennings <56309747+W-Jennings647@users.noreply.github.com>
2024-01-26 01:25:42 +0000

    HW 1 - Creating multiplier

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -6,6 +6,13 @@ int add_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+int multiply_numbers(int num1)
+{
+	int num2 = 5;
+	int return_value = num1 * num2;
+	return return_value;
+}
+
 //write function code here
 
 
