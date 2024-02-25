273d181bd34572569d4402eef22e3a5ec4a8f6c3
William Jennings <56309747+W-Jennings647@users.noreply.github.com>
2024-02-25 00:23:45 +0000

    Homework 4 - Repetition

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,67 @@
 //write include statements
+#include <iostream>
+#include "repetition.h"
+
 
 //write using statements
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	//declaring
+	int input1 = 0;
+	int input2 = 0;
+	int result = 0;
+	char control = 'z';
+	int choice = 0;
+
+	//user input
+
+	//calling functions
+
+	do
+	{
+		menu_start();
+		cin>>choice;
+		switch(choice)
+		{
+			case 1:
+				cout<<"Please enter the value you would like to find the factorial of: ";
+				cin>>input1;
+				result = factorial(input1);
+				cout<<"The factorial of ";
+				cout<<input1;
+				cout<<" is ";
+				cout<<result;
+				cout<<"\n";
+				break;
+			case 2:
+				cout<<"Please enter the 2 values you would like to find the greatest common divisor of: \n";
+				cin>>input1;
+				cin>>input2;
+				result = gcd(input1, input2);
+				cout<<"The greatest common divisor of ";
+				cout<<input1;
+				cout<<" and ";
+				cout<<input2;
+				cout<<" is ";
+				cout<<result;
+				cout<<"\n";
+				break;
+			case 3:
+				cout<<"Are you sure? If you would really like to exit, enter Y \n";
+				cin>>control;
+				break;
+			default:
+				cout<<"Invalid input \n";
+				break;
+				
+
+		}
+	}while(!(control == 'y' || control == 'Y'));
+
+	
+
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,51 @@
 //add include statements
+#include "repetition.h"
+#include "if_else_if.h"
+
+using std::cout;
+
 
 //add function(s) code here
+int factorial(int num)
+{
+    int result = 1;
+    while(num != 1)
+    {
+        result = result * num;
+        num--;
+
+    }
+    return result;
+
+}
+
+int gcd(int num1, int num2)
+{
+    int holding = 0;  
+    
+    while(!(num1 == num2))
+    {
+        if(num1 < num2)
+        {
+            holding = num1;
+            num1 = num2;
+            num2 = holding;
+        }
+        else if(num1 > num2)
+        {
+            num1 = num1 - num2;
+        }
+    }
+
+    return num1;
+    
+}
+
+int menu_start()
+{
+	cout<<"Would you like to: \n";
+	cout<<"1 - Find a factorial \n";
+	cout<<"2 - Find a greatest common divisor \n";
+	cout<<"3 - Exit \n";
+    return 0;
+}
